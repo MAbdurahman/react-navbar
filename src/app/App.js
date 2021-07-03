28cea380b752d515eac4e4c9701c529293026728
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-03 05:26:28 -0500

    Tenative solution for responsive navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from '../components/navigation/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+    <Navbar />
     </div>
   );
 }
